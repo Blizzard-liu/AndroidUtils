03dcee681830dcd9b8f25f95415f1c3cd6f62305
liubo <lb1095796478@163.com>
2017-05-09 11:17:14 +0800

    增加gsonUtil

---
+++ utils/src/main/java/com/example/utils/GsonUtil.java
@@ -0,0 +1,114 @@
+package com.example.utils;
+
+import android.text.TextUtils;
+
+import com.google.gson.Gson;
+import com.google.gson.reflect.TypeToken;
+
+import org.json.JSONException;
+import org.json.JSONObject;
+
+import java.lang.reflect.Type;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
+
+/**
+ * @创建者 Blizzard-liu
+ * @创建时间 2017/5/9 11:04
+ * @描述 ${TODO}
+ * @更新者 ${Author}
+ * @更新时间 2017/5/9
+ * @更新描述 ${TODO}
+ */
+
+public class GsonUtil {
+
+    /**
+     * 把一个map变成json字符串
+     * @param map
+     * @return
+     */
+    public static String parseMapToJson(Map<?, ?> map) {
+        try {
+            Gson gson = new Gson();
+            return gson.toJson(map);
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+        return null;
+    }
+
+    /**
+     * 把一个json字符串变成对象
+     * @param json
+     * @param cls
+     * @return
+     */
+    public static <T> T parseJsonToBean(String json, Class<T> cls) {
+        Gson gson = new Gson();
+        T t = null;
+        try {
+            t = gson.fromJson(json, cls);
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+        return t;
+    }
+
+    /**
+     * 把json字符串变成map
+     * @param json
+     * @return
+     */
+    public static HashMap<String, Object> parseJsonToMap(String json) {
+        Gson gson = new Gson();
+        Type type = new TypeToken<HashMap<String, Object>>() {
+        }.getType();
+        HashMap<String, Object> map = null;
+        try {
+            map = gson.fromJson(json, type);
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+        return map;
+    }
+
+    /**
+     * 把json字符串变成集合
+     * params: new TypeToken<List<yourbean>>(){}.getType(),
+     *
+     * @param json
+     * @param type  new TypeToken<List<yourbean>>(){}.getType()
+     * @return
+     */
+    public static List<?> parseJsonToList(String json, Type type) {
+        Gson gson = new Gson();
+        List<?> list = gson.fromJson(json, type);
+        return list;
+    }
+
+    /**
+     *
+     * 获取json串中某个字段的值，注意，只能获取同一层级的value
+     *
+     * @param json
+     * @param key
+     * @return
+     */
+    public static String getFieldValue(String json, String key) {
+        if (TextUtils.isEmpty(json))
+            return null;
+        if (!json.contains(key))
+            return "";
+        JSONObject jsonObject = null;
+        String value = null;
+        try {
+            jsonObject = new JSONObject(json);
+            value = jsonObject.getString(key);
+        } catch (JSONException e) {
+            e.printStackTrace();
+        }
+        return value;
+    }
+}
